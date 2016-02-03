81b8efb899b3d640b765d24aa65cc7c9e097fe62
Matt Song <matt.song@live.cn>
2016-02-03 16:52:19 +0800

    Updated at 2016-02-03

--- L05_structs_unions_and_bitfields/choose_element_union.c
+++ L05_structs_unions_and_bitfields/choose_element_union.c
@@ -1,14 +1,51 @@
 #include <stdio.h>
 
+typedef enum 
+{
+    COUNT, POUNDS, PINTS
+} unit_of_measure;
+
 typedef union 
 {
+    short count;
     float weight;
-    int count;
-} cupcake;
+    float volume;
+} quantity;
+
+typedef struct 
+{
+    const char *name;
+    const char *country;
+    quantity amount;
+    unit_of_measure units;
+} fruit_order;
+
+void display(fruit_order order)
+{
+    printf("This order contains ");
+
+    if ( order.units == PINTS)
+    {
+        printf("%2.2f pints of %s\n", order.amount.volume , order.name);
+    }
+    else if ( order.units == POUNDS )
+    {
+        printf("%2.2f lbs of %s\n", order.amount.weight, order.name);
+    }
+    else
+    {
+        printf("%i %s\n", order.amount.count , order.name);
+    }
+}
 
 int main()
 {
-    cupcake order = {2};
-    printf("Cupcakes quantity: %i\n", order.count);
+    fruit_order apples = {"apples", "England", apples.amount.count=144, COUNT};
+    fruit_order strawberries = {"strawberries", "Spain", strawberries.amount.weight =17.6, POUNDS};
+    fruit_order oj = {"orange juice", "U.S.A.", oj.amount.volume=10.5, PINTS};
+    display(apples);
+    display(strawberries);
+    display(oj);
     return 0;
 }
+
